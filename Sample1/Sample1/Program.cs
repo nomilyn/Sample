707a7108bb912329f6c71446ffe965d8b916b05a
Nomilyn Cayton <ncayton4980@conestogac.on.ca>
2020-01-21 22:03:40 -0500

    Updated

--- Sample1/Sample1/Program.cs
+++ Sample1/Sample1/Program.cs
@@ -6,8 +6,12 @@ namespace Sample1
     {
         static void Main(string[] args)
         {
+            int n1 = 2, n2 = 3, sum = 0;
             Console.WriteLine("Hello World!");
             Console.ReadLine();
+            sum = n1 + n2;
+            Console.WriteLine("The sum is : "+sum);
+
         }
     }
 }
