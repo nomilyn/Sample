bc485a5f87bef1f57ffd898f88adf6921fc40834
Nomilyn Cayton <ncayton4980@conestogac.on.ca>
2020-01-21 22:09:29 -0500

    Another change to the message

--- Sample1/Sample1/Program.cs
+++ Sample1/Sample1/Program.cs
@@ -6,9 +6,9 @@ namespace Sample1
     {
         static void Main(string[] args)
         {
-            int n1 = 2, n2 = 3, sum = 0;
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            int n1 = 0, n2 = 0, sum = 0;
+            Console.WriteLine("Enter First Number : "); n1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter First Number : "); n2 = Int32.Parse(Console.ReadLine());
             sum = n1 + n2;
             Console.WriteLine("The sum is : "+sum);
 
